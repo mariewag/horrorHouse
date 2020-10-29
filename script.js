b0a7e4c83e195c7d09f5d89fbc3569b39be22116
Marie Wagle <mariewagle@Maries-MacBook-Pro.local>
2020-10-29 13:47:40 +0100

    made the buttons appear in js

--- script.js
+++ script.js
@@ -4,75 +4,75 @@ let currentScene = 0;
 const scenes = [
     
 {   // Start 0
-    description: "Welcome to Horror House! You are stuck in the house and need to find the hidden key to get out. Do you want to start looking in the kitchen or the living room?",
-    choices: ["kitchen", "living room"],
+    text: "Do you want to start looking in the kitchen or the living room?",
+    choices: ["Kitchen", "Living room"],
     nextScene: [1, 2],
 },
    
 {   //  1
-    description: "A man is standing right in front of you! Do you punch him or scream?",
-    choices: ["punch him", "scream"],
+    text: "A man is standing right in front of you! Do you punch him or scream?",
+    choices: ["Punch him", "Scream"],
     nextScene: [10, 3],
 },
 
 {   //  2
-    description: "There is a zombie in the room! Do you want to hide or run to the kitchen?",
-    choices: ["hide", "run"],
+    text: "There is a zombie in the room! Do you want to hide or run to the kitchen?",
+    choices: ["Hide", "Run"],
     nextScene: [7, 3],
 },
 
 {   //  3
-    description: "You made it to the kitchen. Do you want to look for the key in the cupboards or the drawers?",
-    choices: ["cupboards", "drawers"],
+    text: "You made it to the kitchen. Do you want to look for the key in the cupboards or the drawers?",
+    choices: ["Cupboards", "Drawers"],
     nextScene: [4, 5],
 },
 
 {   // 4
-    description: "Its not there! Do you want to look in the drawers?",
-    choices: ["yes", "no"],
+    text: "Its not there! Do you want to look in the drawers?",
+    choices: ["Yes", "No"],
     nextScene: [5, 9],
 },
 
 {   // 5
-    description: "You found the key! Will you take it with you?",
-    choices: ["yes", "no"],
+    text: "You found the key! Will you take it with you?",
+    choices: ["Yes", "No"],
     nextScene: [6, 8],
 },
 
 {   // 6
-    description: "A ghost is coming for you. Walk or run?",
-    choices: ["walk", "run"],
+    text: "A ghost is coming for you. Walk or run?",
+    choices: ["Walk", "Run"],
     nextScene: [9, 11],
 },
 
 {   // 7
-    description: "Oh no, he found you! Do you want to try again?",
-    choices: ["yes", "no"],
+    text: "Oh no, he found you! Do you want to try again?",
+    choices: ["Yes", "No"],
     nextScene: [0, 0],
 },
 
 {   // 8
-    description: "You won’t surive without the key! Are you sure you don't want to take it with you?",
-    choices: ["yes", "no"],
+    text: "You won’t surive without the key! Are you sure you don't want to take it with you?",
+    choices: ["Yes", "No"],
     nextScene: [9, 6],
 },
 
 {   // 9
-    description: "Oh no, you died! Do you want to try again?",
-    choices: ["yes", "no"],
+    text: "Oh no, you died! Do you want to try again?",
+    choices: ["Yes", "No"],
     nextScene: [0, 0],
 },
 
 
 {   // 10
-    description: "Oh no, you're too weak! Do you want to try again?",
-    choices: ["yes", "no"],
+    text: "Oh no, you're too weak! Do you want to try again?",
+    choices: ["Yes", "No"],
     nextScene: [0, 0],
 },
 
 {   // 11
-    description: "You made it! Well done! Go to start?",
-    choices: ["yes", "no"],
+    text: "You made it! Well done! Go to start?",
+    choices: ["Yes", "No"],
     nextScene: [0, 0],
 }
 ];
@@ -81,7 +81,63 @@ const scenes = [
 // Run our application
 window.onload = presentScene;
 
+
+// Define the actions for the application
+function presentScene() {
+    const scene = scenes[currentScene];
+    updateDescription(scene);
+    createChoices(scene);
+}
+
+function updateDescription(scene) {
+    const textTag = document.getElementById("text");
+    textTag.innerText = scene.text;
+}
+
+function createChoices(scene) {
+    const container = document.getElementById("choices-container");
+    container.innerText = "";
+
+for (let i = 0; i < scene.choices.length; i++) {
+    const choice = scene.choices[i];
+    const nextScene = scene.nextScene[i];
+
+    const button = createButton(choice, nextScene);
+    container.append(button);
+    }
+}
+
+function createButton(choice, nextScene) {
+    const button = document.createElement("button");
+
+    button.innerText = choice;
+    button.onclick = function() {
+        handleUserChoice(nextScene);
+    }
+
+    return button;
+}
+
+
+function handleUserChoice(nextScene) {
+    currentScene = nextScene;
+    presentScene();
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
 // Define the actions for our applications
+/*
 function presentScene() {
     const answer = prompt(scenes[currentScene].description);
     handleUserChoice(answer)
@@ -143,3 +199,4 @@ function handleUserChoice(answer) {
     presentScene()
 
 }
+*/
